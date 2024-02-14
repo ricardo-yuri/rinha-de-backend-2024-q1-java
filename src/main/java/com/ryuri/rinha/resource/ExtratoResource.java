03e5b238458b9729a6021ce3d967cdded73ce6a2
ricardo-yuri <ricardo-yuri@outlook.com>
2024-02-14 11:09:19 -0300

    Implements liquibase

---
+++ src/main/java/com/ryuri/rinha/resource/ExtratoResource.java
@@ -0,0 +1,10 @@
+package com.ryuri.rinha.resource;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/extrato")
+public class ExtratoResource {
+}
